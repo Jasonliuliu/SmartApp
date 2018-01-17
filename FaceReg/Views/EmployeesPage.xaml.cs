77fa8e74fef5b40812022b5144b3c96a185882e6
Wang, Charence [ICG-IT] <yw72895@imcap.ap.ssmb.com>
2018-01-17 16:12:53 +0800

    init

---
+++ FaceReg/Views/EmployeesPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FaceReg
+{
+    public partial class EmployeesPage : ContentPage
+    {
+        public EmployeesPage()
+        {
+            InitializeComponent();
+
+
+            BindingContext = new ViewModels.EmployeesViewModel();
+        }
+    }
+}
---
+++ FaceReg/Views/NewItemPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FaceReg.Views
+{
+    public class NewItemPage : ContentPage
+    {
+        public NewItemPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
